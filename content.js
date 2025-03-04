841fadf5b42b56fbe825ce497f19bc6f3ee73ca9
JJKSweaty <johnkoper12@gmail.com>
2025-03-03 23:39:24 -0500

    MediaSesh Initial

--- content.js
+++ content.js
@@ -1,2 +1,12 @@
 navigator.mediaSession.metadata = new MediaMetadata
-   
+
+const mediaSession = navigator.mediaSession;
+
+mediaTitle = navigator.mediaSession.metadata.title;
+mediaArtist = navigator.mediaSession.metadata.artist;
+mediaAlbum = navigator.mediaSession.metadata.album;
+mediaArtwork = navigator.mediaSession.metadata.artwork;
+mediaArtworkSizes = navigator.mediaSession.metadata.artwork.sizes;
+mediaArtworkSrc = navigator.mediaSession.metadata.artwork.src;
+
+
